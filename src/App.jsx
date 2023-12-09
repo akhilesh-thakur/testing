7cde6a4bddc980c00e13f10596998f0b2719e9a0
Akhilesh <53186185+akhilesh-thakur@users.noreply.github.com>
2023-12-09 11:11:42 +0530

    configured the existing project and added Tailwind css to it with a dummy navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-      Hello World!
+    <Navbar/>
     </>
+    
   )
 }
 
